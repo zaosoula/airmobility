eb4c7fc81b4faf196c065f4ad19b9ce8e5255d5a
Zao Soula <contact@zaosoula.fr>
2021-02-02 19:27:15 +0100

    css fixes & responsiveness

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  // smoothscroll.polyfill();
   let form = document.querySelector('#contactform');
 
   setTimeout(function() {
     form.action = "mailto:alice.marty@ynov.com";
   },1000);
-  
+
+  let navLinks = document.querySelectorAll('nav a')
+
+  navLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+      e.preventDefault();
+
+      navLinks.forEach(function(_link) {
+        _link.classList.remove('active');
+      })
+      
+      link.classList.add('active');
+      let target = document.querySelector(link.attributes.getNamedItem('data-target').value);
+
+      target.scrollIntoView({ behavior: 'smooth' });
+
+    })
+  })
 })
